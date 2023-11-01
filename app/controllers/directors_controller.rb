d0c783c821dcabea624a05540b376691202a9b23
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-01 15:57:57 +0000

    adding director pages

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,4 +3,13 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list"})
   end
+
+  def show
+    the_id = params.fetch("the_id")
+    
+    @x = Director.where({:id=>the_id})
+
+    render({ :template => "director_templates/details"})
+  end
+
 end
